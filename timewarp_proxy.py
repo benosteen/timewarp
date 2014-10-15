b87779dd8b11d573b2c0a8e625b710e64413006c
Ben O'Steen <bosteen@gmail.com>
2014-10-15 13:04:51 +0100

    Uses Memento 'Accept-Datetime' header
    Converts the time request from the accept-datetime header into something the web archive would understand.

--- timewarp_proxy.py
+++ timewarp_proxy.py
@@ -1,12 +1,19 @@
 import time
 from libmproxy.script import concurrent
 
+from dateutil.parser import parse as dateutil_parser
+
 @concurrent
 def request(context, flow):
     #print "handle request: %s - %s" % (flow.request.host, flow.request.path)
     # http://web.archive.org/web/19981212031357
+    fluxcapacitor_setting = "19981201052808"
+    if flow.request.headers["accept-datetime"]:
+        # eg Accept-Datetime: Thu, 31 May 2007 20:35:00 GMT
+        parsed_date = dateutil_parser(flow.request.headers["accept-datetime"])
+        fluxcapacitor_setting = parsed_date.strftime("%Y%m%d%H%M%S")
     if not "id_/http" in flow.request.path:
-	    flow.request.path = "/web/19981201052808id_/http://%s%s" % (flow.request.host, flow.request.path)
+        flow.request.path = "/web/%sid_/http://%s%s" % (fluxcapacitor_setting, flow.request.host, flow.request.path)
     flow.request.scheme = "http"
     flow.request.host = "web.archive.org"
     flow.request.headers["Host"] = ["web.archive.org"]
